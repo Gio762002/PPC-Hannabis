4d3c06d4f9ef5759d7755a9aed8b263a68a69d9f
Replit user <>
2024-01-29 15:38:11 +0000

    rewrote shm

--- src/game.py
+++ src/game.py
@@ -1,12 +1,10 @@
-import random
-from multiprocessing import Process, Queue, shared_memory, Pool
+from multiprocessing import Process
 import concurrent.futures
-import signal
-import sys
 import socket
 import select
-import numpy as np
 import struct
+import sysv_ipc
+import random
 
 
 class Game:
@@ -21,71 +19,25 @@ class Game:
         self.deck = []
         # self.shuffle_deck()
         self.create_shared_memory()
-        print(self.shm_names)
 
     def create_shared_memory(self):
-        card_type = np.dtype([('color', np.uint8), ('number', np.uint8)])
-        discard_pile = np.zeros(self.number_of_players * 10, dtype=card_type)
-        suits_in_construction = np.zeros([self.number_of_players, 10],
-                                         dtype=card_type,
-                                         order='C')
-        suits_completed = np.zeros(self.number_of_players, dtype=np.uint8)
-        information_tokens = np.zeros(1, dtype=np.uint8)
-        information_tokens[0] = self.number_of_players + 3
-        fuse_tokens = np.zeros(1, dtype=np.uint8)
-        fuse_tokens[0] = 3
+        self.shm_pool = []
+        self.suits = sysv_ipc.SharedMemory(300, sysv_ipc.IPC_CREX,
+                                           5 * self.number_of_players)
+        self.shm_pool.append(self.suits)
 
-        try:
-            discard_pile_buf = shared_memory.SharedMemory(
-                create=True, size=discard_pile.nbytes)
-            suits_in_construction_buf = shared_memory.SharedMemory(
-                create=True, size=suits_in_construction.nbytes)
-            suits_completed_buf = shared_memory.SharedMemory(
-                create=True, size=suits_completed.nbytes)
-            information_tokens_buf = shared_memory.SharedMemory(
-                create=True, size=information_tokens.nbytes)
-            fuse_tokens_buf = shared_memory.SharedMemory(
-                create=True, size=fuse_tokens.nbytes)
-        except Exception as e:
-            print("Error creating shared memory buffers: ", e)
+        self.information_tokens = sysv_ipc.SharedMemory(
+            301, sysv_ipc.IPC_CREX, 1)
+        itok = self.number_of_players + 3
+        self.information_tokens.write(struct.pack('i', itok))
+        self.shm_pool.append(self.information_tokens)
 
-        try:
-            self.discard_pile = np.ndarray(discard_pile.shape,
-                                           dtype=discard_pile.dtype,
-                                           buffer=discard_pile_buf.buf)
-            self.suits_in_construction = np.ndarray(
-                suits_in_construction.shape,
-                dtype=suits_in_construction.dtype,
-                buffer=suits_in_construction_buf.buf)
-            self.suits_completed = np.ndarray(suits_completed.shape,
-                                              dtype=suits_completed.dtype,
-                                              buffer=suits_completed_buf.buf)
-            self.information_tokens = np.ndarray(
-                information_tokens.shape,
-                dtype=information_tokens.dtype,
-                buffer=information_tokens_buf.buf)
-            self.fuse_tokens = np.ndarray(fuse_tokens.shape,
-                                          dtype=fuse_tokens.dtype,
-                                          buffer=fuse_tokens_buf.buf)
-        except Exception as e:
-            print("Error creating shared memory buffers: ", e)
+        self.fuse_tokens = sysv_ipc.SharedMemory(302, sysv_ipc.IPC_CREX, 1)
+        self.fuse_tokens.write(b'3')
+        self.shm_pool.append(self.fuse_tokens)
 
-        self.shm_pool = [
-            discard_pile_buf, suits_in_construction_buf, suits_completed_buf,
-            information_tokens_buf, fuse_tokens_buf
-        ]
-        self.shm_names = {
-            "discard_pile": discard_pile_buf.name,
-            "suits_in_construction": suits_in_construction_buf.name,
-            "suits_completed": suits_completed_buf.name,
-            "information_tokens": information_tokens_buf.name,
-            "fuse_tokens": fuse_tokens_buf.name
-        }
-
-    def delete_shared_memory(self):
         for shm in self.shm_pool:
-            shm.close()
-            shm.unlink()
+            shm.attach()
 
     def socket_connection(self):
         HOST = "localhost"
@@ -115,20 +67,24 @@ class Game:
             except Exception as e:
                 print("Error sending basic infos: ", e)
 
-    def handle_sigint(self, signal, frame):
-        if self.gaming:
-            print("releasing shared memory before exiting")
-            self.gaming = False
-            try:
-                self.delete_shared_memory()
-                exit(0)
-            except Exception as e:
-                print("")
+    def shuffle_deck(self):
+        reserved_suit = [1, 1, 1, 2, 2, 3, 3, 4, 4, 5]
+        reserved_deck = [(i, j) for i in range(self.number_of_players)
+                         for j in reserved_suit]
+        #card = (color,number)
+        random.shuffle(reserved_deck)
+        self.deck = reserved_deck
+
+    def distribute_card(self, init=False):
+        """distibute a card to a player, if init is True, distribute 5 cards to each player, means the game is just started"""
+        if init:
+            return [self.deck.pop(0) for i in range(5)]
+        else:
+            return self.deck.pop(0)
 
 
 if __name__ == "__main__":
     game = Game(2)
-    signal.signal(signal.SIGINT, game.handle_sigint)
     # game.run()
     processes = []
     processes.append(Process(target=game.socket_connection, args=()))
